2fd0add5838efe7e95f44973e61cb6458d4d5069
Kevin Du <kevind7@cs.utexas.edu>
2017-09-18 16:44:01 -0500

    toy lights and entities added

--- A1setup/TutorialApplication.cpp
+++ A1setup/TutorialApplication.cpp
@@ -29,8 +29,44 @@ TutorialApplication::~TutorialApplication(void)
 //---------------------------------------------------------------------------
 void TutorialApplication::createScene(void)
 {
-    // Create your scene here :)
+    // Ambient light
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0, 0, 0));
+    mSceneMgr->setShadowTechnique(Ogre::SHADOWTYPE_STENCIL_ADDITIVE);
+    //Set camera
+    mCamera->setPosition(0, 47, 222);   //Pull camera up and back
+    // Light sources
+    Ogre::Light * light = mSceneMgr->createLight("MainLight");
+    light->setPosition(20, 80, 50);
+
+    //Plane
+    // Ogre::Plane plane(Ogre::Vector3::UNIT_Y, 0);
+    // Ogre::MeshManager::getSingleton().createPlane(
+    //     "ground",
+    //     Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+    //     plane, 1500, 1500, 20, 20, true, 1, 5, 5, Ogre::Vector3::UNIT_Z);
+    // Ogre::Entity* groundEntity = mSceneMgr->createEntity("ground");
+    // groundEntity->setCastShadows(false);
+    // groundEntity->setMaterialName("Examples/Rockwall"); //may break
+    // mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(groundEntity);
+
+    //Entity
+    // Ogre::Entity* ogreHead = mSceneMgr->createEntity("Head", "ogrehead.mesh");
+    // ogreHead->setCastShadows(true);
+    // mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ogreHead);
+
+    Ogre::Entity* ogreHead2 = mSceneMgr->createEntity("Head2", "ogrehead.mesh");
+    Ogre::SceneNode* headNode2 = mSceneMgr->getRootSceneNode()->createChildSceneNode(
+        Ogre::Vector3(84, 48, 0));  //Setting position ourselves
+    headNode2->attachObject(ogreHead2);
+
+    //Normals? Texture? something not right
+    Ogre::Entity* ball = mSceneMgr->createEntity("Ball", "sphere.mesh");
+    ball->setCastShadows(true);
+    mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ball);
+    ball->setMaterialName("droplet.png");
+
 }
+
 //---------------------------------------------------------------------------
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
